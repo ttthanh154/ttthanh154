a8957a21459fa90f24e5bf4bbc9046ca5094869b
Gold <ttthanh154@gmail.com>
2022-03-30 23:49:21 +0700

    dá

---
+++ js/cart.js
@@ -0,0 +1,15 @@
+// function updateCart(){
+//     var cart_item = document.getElementsByClassName("cart-items")[0];
+//     var cart_rows = cart_item.getElementsByClassName("cart-row");
+//     var total = 0;
+//     for (var i = 0; i < cart_rows.length; i++){
+//         var cart_row = cart_row[i];
+//         var price_item = cart_row.getElementsByClassName("cart-price")[0];
+//         var quantity_item = cart_row.getElementsByClassName("cart-quantity-input")[0];
+//         var price = parseFloat(price_item.innerText);
+//         var quantity = quantity_item.value;
+//         total = total + (price * quantity);
+//     }
+//     document.getElementsByClassName("cart-total-price")[0].innerText = total;
+// }
+
--- js/product.js
+++ js/product.js
@@ -3,3 +3,12 @@ function changeImage(id){
     console.log(imagePath);
     document.getElementById('preference-image').setAttribute('src',imagePath);
 }
+
+
+function changeColor(type,element) {
+    let color = document.getElementsByClassName('color-img');
+    for (i = 0; i < color.length; i++) {
+        color[i].style.border = 'none';
+    };
+    element.style.border = '1px solid black';
+} 
